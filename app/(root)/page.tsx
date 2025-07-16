55c18615e0aeae39ea8f6be4647cf12c35245c94
Ayman <aymanibrahim678@gmail.com>
2025-07-16 11:17:35 +0300

    Real-time Search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,10 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
-  const {data: posts} = await sanityFetch({ query: STARTUPS_QUERY })
+  const params = { search: query || null };
+
+
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
   return (
     <>
       <section className="pink_container">
@@ -26,7 +29,7 @@ export default async function Home({
       </section>
       <section className="section_container">
         <p className="text-30-semibold">
-          {query ? `Search results for ${query}` : "All Startups"}
+          {query ? `Search results for "${query}"` : "All Startups"}
         </p>
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
