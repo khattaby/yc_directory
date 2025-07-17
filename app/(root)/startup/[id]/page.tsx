f53cdb75e30a837e9ac65c8f3106c9ecfcc42e66
Ayman <aymanibrahim678@gmail.com>
2025-07-17 05:25:16 +0300

    parallel fetching and playlist feature

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,41 +1,58 @@
-import { formatDate } from "@/lib/utils";
+import { Suspense } from "react";
 import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
-import Image from "next/image";
-import Link from "next/link";
+import {
+  PLAYLIST_BY_SLUG_QUERY,
+  STARTUP_BY_ID_QUERY,
+} from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
-import React, { Suspense } from "react";
+import { formatDate } from "@/lib/utils";
+import Link from "next/link";
+import Image from "next/image";
+
 import markdownit from "markdown-it";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 const md = markdownit();
-export const experimental = true;
 
-const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+export const experimental_ppr = true;
+
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  const [post, editorData] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks-new",
+    }),
+  ]);
+
   if (!post) return notFound();
+
+  const editorPosts = editorData?.select || [];
   const parsedContent = md.render(post?.pitch || "");
+
   return (
     <>
       <section className="pink_container !min-h-[230px]">
-        <p className=" bg-amber-300 px-6 py-3 font-work-sans font-bold rounded-sm uppercase relative tag-tri">
-          {formatDate(post?._createdAt)}{" "}
-        </p>
+        <p className="tag">{formatDate(post?._createdAt)}</p>
+
         <h1 className="heading">{post.title}</h1>
         <p className="sub-heading !max-w-5xl">{post.description}</p>
       </section>
-      <section className="section-container py-10 px-6">
+
+      <section className="section_container">
         <img
           src={post.image}
           alt="thumbnail"
           className="w-full h-auto rounded-xl"
         />
+
         <div className="space-y-5 mt-10 max-w-4xl mx-auto">
           <div className="flex-between gap-5">
             <Link
-              href={`user/${post.author?._id}`}
+              href={`/user/${post.author?._id}`}
               className="flex gap-2 items-center mb-3"
             >
               <Image
@@ -45,15 +62,18 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
                 height={64}
                 className="rounded-full drop-shadow-lg"
               />
+
               <div>
                 <p className="text-20-medium">{post.author.name}</p>
                 <p className="text-16-medium !text-black-300">
-                  {post.author.username}
+                  @{post.author.username}
                 </p>
               </div>
             </Link>
+
             <p className="category-tag">{post.category}</p>
           </div>
+
           <h3 className="text-30-bold">Pitch Details</h3>
           {parsedContent ? (
             <article
@@ -64,14 +84,27 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
             <p className="no-result">No details provided</p>
           )}
         </div>
+
         <hr className="divider" />
-        {/*TODO: EDITOR SELECTED STARTUPS*/}
-        <Suspense fallback={<Skeleton className="view-skeleton"/>}>
-            <View id={id}/>
+
+        {editorPosts?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
+
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupTypeCard, i: number) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
+
+        <Suspense fallback={<Skeleton className="view_skeleton" />}>
+          <View id={id} />
         </Suspense>
       </section>
     </>
   );
 };
 
-export default page;
+export default Page;
